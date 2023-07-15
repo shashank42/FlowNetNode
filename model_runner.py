b8d4734f3ba41b11e1d6c498709f8da80c84d178
Shashank Srivastava <devo.shanky@gmail.com>
2023-07-15 23:09:22 +0530

    register

--- model_runner.py
+++ model_runner.py
@@ -8,7 +8,7 @@ def run_model(prompt, request_id):
     fname = infer(prompt, request_id)    
     print("Got back inference, now make call to contract")
     
-    cmd = f'flow transactions send cadence/transactions/submit_inference.cdc {request_id} "{url}" --network=testnet --signer=testnet-account'
+    cmd = f'flow transactions send cadence/transactions/submit_inference.cdc {request_id} "{fname}" --network=testnet --signer=testnet-account'
     print(cmd)
     arch = subprocess.check_output(cmd, shell=True)
     print(arch)
